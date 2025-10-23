ffb25bac8133045c51a57f1e1774770fb760ce3a
Divakar <divakarchadalavada@gmail.com>
2025-10-23 17:26:54 +1100

    Updated some components and fixed bugs

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -9,7 +9,7 @@ export const About = () => {
     "Vue",
   ];
 
-  const backendSkills = ["Java", "AWS", "SpringBoot", "MySQL", "Node.js"];
+  const backendSkills = ["Java", "AWS", "SpringBoot", "SQL", "Node.js"];
 
   return (
     <section
@@ -31,7 +31,7 @@ export const About = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+                <h3 className="text-xl font-bold mb-4"> Frontend</h3> 
                 <div className="flex flex-wrap gap-2">
                   {frontendSkills.map((tech, key) => (
                     <span
@@ -53,9 +53,7 @@ export const About = () => {
                     <span
                       key={key}
                       className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
-                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
-                    "
-                    >
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
                       {tech}
                     </span>
                   ))}
@@ -65,20 +63,7 @@ export const About = () => {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
-              <ul className="list-disc list-inside text-gray-300 space-y-2">
-                <li>
-                  <strong> B.Tech in Elecetonics and Communication Engineering </strong>
-                   - Swarnandhra College
-                  (2021-2025)
-                </li>
-                <li>
-                  Relevant Coursework: Java Full Stack Development - IIT Guwahati, Data analysis 
-                  - IBM, IOT - Teachnooks, AWS...
-                </li>
-              </ul>
-            </div>
+
             <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
               <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
               <div className="space-y-4 text-gray-300">
@@ -105,6 +90,22 @@ export const About = () => {
                 </div>
               </div>
             </div>
+
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
+              <ul className="list-disc list-inside text-gray-300 space-y-2">
+                <li>
+                  <strong> B.Tech in Elecetonics and Communication Engineering </strong>
+                   - Swarnandhra College
+                  (2021-2025)
+                </li>
+                <li>
+                  Relevant Coursework: Java Full Stack Development - IIT Guwahati, Data analysis 
+                  - IBM, IOT - Teachnooks, AWS...
+                </li>
+              </ul>
+            </div>
+            
           </div>
         </div>
       </RevealOnScroll>
