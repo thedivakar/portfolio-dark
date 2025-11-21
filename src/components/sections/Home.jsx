57a9263fd152742ed62189b66cf493e937c30bc3
Divakar <144816507+thedivakar@users.noreply.github.com>
2025-11-21 14:38:54 +1100

    Update Home.jsx

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -20,7 +20,7 @@ export const Home = () => {
           </p>
           <div className="flex justify-center space-x-4">
             <a
-              href="https://drive.google.com/file/d/1AsDxtc3QyYerJFQMRub1y88xZO4i24cS/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1jlEJvL45GXgHdiSmHspf4T8f9X5Lqmgd/view?usp=drive_link"
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
             >
               Resume
